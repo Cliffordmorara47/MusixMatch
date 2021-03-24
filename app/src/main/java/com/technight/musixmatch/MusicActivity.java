143c7b31004e12251f8e3b6141573e7dbf0db6e0
Cliffordmorara47 <cliffordmorara45@gmail.com>
2021-03-24 17:11:36 +0300

    Displayed song list in a list view

--- app/src/main/java/com/technight/musixmatch/MusicActivity.java
+++ app/src/main/java/com/technight/musixmatch/MusicActivity.java
@@ -3,12 +3,37 @@ package com.technight.musixmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindDimen;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MusicActivity extends AppCompatActivity {
+    public static final String TAG = MusicActivity.class.getSimpleName();
+    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.lyricsTextView) TextView mLyricsTextView;
+    @BindView(R.id.lv) TextView mLv;
+
+    private String[] songs = new String[] {"Memories", "Quite Miss Home", "Sweet but Psycho", "You are the Reason",
+            "EastSide", "TapOut", "Sex Sounds", "Back Door", "7 Summers", "Think about things", "Good News",
+            "Little Nokia", "Yo Perreo Sola", "WAP", "People I've Been Sad", "Long Road Home", "Back Door",
+            "Move On", "Go Crazy", "Hug the Streets"};
+
+    private String[] artists = new String[] {"Maroon 5", "James Arthur", "Ava Max", "Calcum Scott", "Benny Blanco, " +
+            "Khalid & Halsey", "Lil Wayne, Nicki Minaj, Future", "Lil Tjay", "Stray Kids", "Morgan Wallen", "Daoi Freyr",
+            "Mac Miller", "Bree Runway", "Bad Bunny, Ivy Queen and Nesi", "Cardi B ft. Megan Thee Stallion", "Christine and the Queens",
+            "Oneohtrix Point Never", "Stray Kids", "Lil Tjay", "Chris Brown, Young Thug", "Rick Ross"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+        ButterKnife.bind(this);
+        mLyricsTextView = (TextView) findViewById(R.id.lyricsTextView);
     }
+
+
 }
--- app/src/main/java/com/technight/musixmatch/MusicListAdapter.java
+++ app/src/main/java/com/technight/musixmatch/MusicListAdapter.java
@@ -1,4 +1,32 @@
 package com.technight.musixmatch;
 
-public class MusicListAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MusicListAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mSongs;
+    private String[] mArtists;
+
+    public MusicListAdapter(Context context, int resource, String[] mSongs, String[] mArtists) {
+        super(context, resource);
+        this.mContext = context;
+        this.mSongs = mSongs;
+        this.mArtists = mArtists;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String song = mSongs[position];
+        String artist = mArtists[position];
+        return String.format("%s /n %s", song, artist);
+    }
+
+    @Override
+    public int getCount() {
+        return mSongs.length;
+    }
 }
