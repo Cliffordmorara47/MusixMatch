2c0f33d9bc204b1d9474e21c7b8e3838f60ac182
Cliffordmorara47 <cliffordmorara45@gmail.com>
2021-03-24 09:15:47 +0300

    Implemented onclick listener

--- app/src/main/java/com/technight/musixmatch/MainActivity.java
+++ app/src/main/java/com/technight/musixmatch/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.exploreButton) Button mExploreButton;
+    @BindView(R.id.appNameTextView) Button mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -20,5 +29,10 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.options_menu, menu);
 
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
