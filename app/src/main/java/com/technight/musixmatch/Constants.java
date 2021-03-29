b77f9e4eb8c7bed4e7c40eb54a2c0eb076c59b46
Cliffordmorara47 <cliffordmorara45@gmail.com>
2021-03-29 23:17:39 +0300

    Created contants class to hold Api credentials

--- app/src/main/java/com/technight/musixmatch/Constants.java
+++ app/src/main/java/com/technight/musixmatch/Constants.java
@@ -1,4 +1,6 @@
 package com.technight.musixmatch;
 
 public class Constants {
+    public static final String MUSIC_BASE_URL = "https://api.musixmatch.com/ws/1.1/";
+    public static final String MUSIC_API_KEY = BuildConfig.MUSIC_API_KEY;
 }
