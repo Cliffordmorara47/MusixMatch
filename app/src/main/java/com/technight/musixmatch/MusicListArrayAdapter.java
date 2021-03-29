3dc42652a6e0e7b79b1025a0beb9018b28e9947f
Cliffordmorara47 <cliffordmorara45@gmail.com>
2021-03-29 23:19:03 +0300

    Cleared errors

--- app/src/main/java/com/technight/musixmatch/MusicListArrayAdapter.java
+++ app/src/main/java/com/technight/musixmatch/MusicListArrayAdapter.java
@@ -1,7 +1,14 @@
 package com.technight.musixmatch;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.technight.musixmatch.MusicActivity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,8 +29,15 @@ public class MusicListArrayAdapter extends ArrayAdapter {
     public Object getItem(int position) {
         String song = mSongs[position];
         String artist = mArtists[position];
+
         return String.format("%s \n -- >%s", song, artist);
     }
+
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//
+//        return super.getView(position, convertView, parent);
+//    }
 
     @Override
     public int getCount() {
