09755c8b45c058cb93ae1d74d304a1de317df8ff
Cliffordmorara47 <cliffordmorara45@gmail.com>
2021-04-07 11:05:35 +0300

    Added location preferences

--- app/src/main/java/com/technight/musixmatch/ui/MainActivity.java
+++ app/src/main/java/com/technight/musixmatch/ui/MainActivity.java
@@ -3,13 +3,16 @@ package com.technight.musixmatch.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.technight.musixmatch.Constants;
 import com.technight.musixmatch.R;
 
 import butterknife.BindView;
@@ -17,8 +20,10 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = "MeetUp";
-    @BindView(R.id.location) EditText mLocation;
+    @BindView(R.id.location) EditText userLocation;
     @BindView(R.id.searchEventButton) Button searchEventButton;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPreferences.edit();
         searchEventButton.setOnClickListener(this);
     }
 
@@ -37,12 +44,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return true;
     }
 
+    private void addLocationToPreferences(String location) {
+        editor.putString(Constants.USER_LOCATION_KEY, location).apply();
+    }
+
     @Override
     public void onClick(View view) {
         if (view == searchEventButton) {
-            String location = mLocation.getText().toString();
+            String location = userLocation.getText().toString();
             Intent intent = new Intent(MainActivity.this, EventsListActivity.class);
             intent.putExtra("location", location);
+            addLocationToPreferences(location);
             startActivity(intent);
         }
     }
