fbfa3a75ec1d5cc452112bf1920ff3a153de5dfe
Cliffordmorara47 <cliffordmorara45@gmail.com>
2021-04-10 00:11:18 +0300

    Cleared errors

--- app/src/main/java/com/technight/musixmatch/ui/LoginActivity.java
+++ app/src/main/java/com/technight/musixmatch/ui/LoginActivity.java
@@ -2,15 +2,35 @@ package com.technight.musixmatch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.technight.musixmatch.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerText) TextView registerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        registerText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == registerText) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccount.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
