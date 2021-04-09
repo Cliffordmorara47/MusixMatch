4e9bb9ec6e0756387f8d3f4db6d47474b016e56b
Cliffordmorara47 <cliffordmorara45@gmail.com>
2021-04-09 12:26:18 +0300

    Implementing firebase to display bookmarked events

--- app/src/main/java/com/technight/musixmatch/ui/BookmarkedEventsList.java
+++ app/src/main/java/com/technight/musixmatch/ui/BookmarkedEventsList.java
@@ -1,16 +1,93 @@
 package com.technight.musixmatch.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.technight.musixmatch.Constants;
 import com.technight.musixmatch.R;
+import com.technight.musixmatch.adapters.FirebaseEventViewHolder;
+import com.technight.musixmatch.models.Event;
 
-public class SavedRestaurantListActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BookmarkedEventsList extends AppCompatActivity {
+    private DatabaseReference databaseReference;
+    private FirebaseRecyclerAdapter<Event, FirebaseEventViewHolder> firebaseRecyclerAdapter;
+
+    @BindView(R.id.recyclerView) RecyclerView recyclerView;
+    @BindView(R.id.errorTextView) TextView errorTextView;
+    @BindView(R.id.progressBar) ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        ButterKnife.bind(this);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_SAVED_EVENT) ;
+        setUpFirebaseAdapter();
+        hideProgressBar();
+        showEvents();
+    }
+
+    private void setUpFirebaseAdapter() {
+        FirebaseRecyclerOptions<Event> FirebaseRecyclerOptions =
+                new FirebaseRecyclerOptions.Builder<Event>()
+                        .setQuery(databaseReference, Event.class)
+                        .build();
+
+
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Event, FirebaseEventViewHolder>(FirebaseRecyclerOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseEventViewHolder firebaseEventViewHolder, int position, @NonNull Event event) {
+                firebaseEventViewHolder.bindEvent(event);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseEventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.events_list, parent, false);
+                return new FirebaseEventViewHolder(view);
+            }
+        };
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    private void showEvents() {
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseRecyclerAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseRecyclerAdapter != null) {
+            firebaseRecyclerAdapter.stopListening();
+        }
     }
 }
